2d2375632d88085aaeb6f22713b409f21f28fe68
indrapranesh <indrapranesh2111@gmail.com>
2023-08-06 23:36:24 +0530

    fix build error

--- src/pages/unsubscribe.jsx
+++ src/pages/unsubscribe.jsx
@@ -16,7 +16,7 @@ export default function Unsubscribe() {
     <>
     <Container className="min-h-3/4 flex  justify-center">
     <h5 className="pt-24 font-display text-xl tracking-tight text-center text-slate-900 sm:text-3xl">
-    We're sorry to see you go! Thank you for being a part of the Hacker Jobs community. If you ever change your mind, feel free to resubscribe anytime!
+    We are sorry to see you go! Thank you for being a part of the Hacker Jobs community. If you ever change your mind, feel free to resubscribe anytime!
                 </h5>
     </Container>
     </>
