a82b9da993da640068390e22e855d3f0e35726cf
indrapranesh <indrapranesh2111@gmail.com>
2023-08-06 23:35:27 +0530

    add unsubscribe page

---
+++ src/pages/unsubscribe.jsx
@@ -0,0 +1,25 @@
+import { Container } from "@/components/Container";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+
+export default function Unsubscribe() {
+
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.replace('/')
+        }, 10000)
+    },[])
+
+    return (
+    <>
+    <Container className="min-h-3/4 flex  justify-center">
+    <h5 className="pt-24 font-display text-xl tracking-tight text-center text-slate-900 sm:text-3xl">
+    We're sorry to see you go! Thank you for being a part of the Hacker Jobs community. If you ever change your mind, feel free to resubscribe anytime!
+                </h5>
+    </Container>
+    </>
+     
+    )
+}
